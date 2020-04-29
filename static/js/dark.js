11df75666fc116d1949fa8904963d940f5eff886
tristanry <tristan.henry@outlook.com>
2020-04-29 21:31:58 +0200

    Change theme color

--- static/js/dark.js
+++ static/js/dark.js
@@ -4,14 +4,17 @@ function setTheme(mode) {
     if (mode === "dark") {
         darkTheme.disabled = false;
         toggle.className = "fas fa-sun";
+        metaThemeColor.setAttribute("content", "#171717");
     } else if (mode === "light") {
         darkTheme.disabled = true;
         toggle.className = "far fa-moon";
+        metaThemeColor.setAttribute("content", "#FFF");
     }
 }
 
 var toggle = document.getElementById("dark-mode-toggle");
 var darkTheme = document.getElementById("dark-mode-theme");
+var metaThemeColor = document.querySelector("meta[name=theme-color]");
 
 // the default theme is light
 var savedTheme = localStorage.getItem("dark-mode-storage") || "light";
